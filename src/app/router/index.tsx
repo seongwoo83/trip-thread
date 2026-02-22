b37ff4212c0f35441811e8fd598af58fcbcb8961
Wooniverse <jeongseongwoo83@daum.net>
2026-02-22 22:38:13 +0900

    feat: Add Global Header Layout

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -1,14 +1,31 @@
-import { Routes, Route } from "react-router-dom"; //
+import { Routes, Route, Outlet } from "react-router-dom";
+import { AppShell, Container } from "@mantine/core";
 import { HomePage } from "@/pages/home";
 import { NotFoundPage } from "@/pages/_not-found";
 import { TripPage } from "@/pages/trip";
+import { Header } from "@/widgets/header";
+
+const Layout = () => (
+	<AppShell header={{ height: 56 }}>
+		<AppShell.Header>
+			<Header />
+		</AppShell.Header>
+		<AppShell.Main>
+			<Container size="xl">
+				<Outlet />
+			</Container>
+		</AppShell.Main>
+	</AppShell>
+);
 
 export const AppRouter = () => {
 	return (
 		<Routes>
-			<Route path="/" element={<HomePage />} />
-			<Route path="/trip" element={<TripPage />} />
-			<Route path="*" element={<NotFoundPage />} />
+			<Route element={<Layout />}>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/trip" element={<TripPage />} />
+				<Route path="*" element={<NotFoundPage />} />
+			</Route>
 		</Routes>
 	);
 };
