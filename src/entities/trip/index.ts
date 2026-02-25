925851414099b82dbf0dd2d937ec3afa205f4a06
Wooniverse <jeongseongwoo83@daum.net>
2026-02-25 23:44:00 +0900

    fix: Refine Create Trip Modal

--- src/entities/trip/index.ts
+++ src/entities/trip/index.ts
@@ -1,3 +1,3 @@
-export type { Trip } from "@/entities/trip/model/types";
+export type { Trip, TripMember } from "@/entities/trip/model/types";
 export { useMyTrips } from "@/entities/trip/model/useMyTrips";
 export { getLocalTripIds, addLocalTripId } from "@/entities/trip/model/localStorage";
