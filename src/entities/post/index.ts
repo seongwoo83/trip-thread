b8a2510fdb558a98291fcd260e15edb4105d04e0
Wooniverse <jeongseongwoo83@daum.net>
2026-03-17 22:40:25 +0900

    feat: Add Infinite Scrolling

--- src/entities/post/index.ts
+++ src/entities/post/index.ts
@@ -2,3 +2,4 @@ export type { Post, PostAuthor, PostWithMeta } from "./model/types";
 export { usePosts } from "./model/usePosts";
 export { usePost } from "./model/usePost";
 export { useRealtimePosts } from "./model/useRealtimePosts";
+export { useInfinitePosts } from "./model/useInfinitePosts";
